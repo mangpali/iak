3b23caad86069294875d43702b8d2fd2387bd001
Mangpali <mangpali29@gmail,com>
2017-05-07 11:54:33 +0800

    recyler view awal

---
+++ app/src/main/java/com/unud/it/mangpali/androidkejar/adapter/ListForecastAdapter.java
@@ -0,0 +1,40 @@
+package com.unud.it.mangpali.androidkejar.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.unud.it.mangpali.androidkejar.R;
+import com.unud.it.mangpali.androidkejar.model.DummyForecast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Mangpali on 5/7/2017.
+ */
+
+public class ListForecastAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private List<DummyForecast>list = new ArrayList<>();
+
+    public ListForecastAdapter(List<DummyForecast> list) {
+        this.list = list;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ForecastItemViewHolder(LayoutInflater.from(parent.getContext())
+        .inflate(R.layout.row_item,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        ((ForecastItemViewHolder)holder).bind(list.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+}
